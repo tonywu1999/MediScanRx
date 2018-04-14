912aea27f702519cefa4048a24b0a086e0e869ff
Tony Wu <sushiboyaw@gmail.com>
2018-04-14 11:43:39 -0400

    Created the tabs

--- MediScan/ViewController.swift
+++ MediScan/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func side_effect(_ sender: UIButton) {
+    
+    }
+    @IBAction func drug_name(_ sender: UITextField) {
+        
+    }
+    
 }
 
---
+++ MediScan/drugs.swift
@@ -0,0 +1,14 @@
+//
+//  drugs.swift
+//  MediScan
+//
+//  Created by Tony Wu on 4/14/18.
+//  Copyright © 2018 Erlun Lian. All rights reserved.
+//
+
+import Foundation
+
+
+class Drugs {
+    
+}
