f90039128a17a6d24ee285d2741d4d45509ada03
Tony Wu <sushiboyaw@gmail.com>
2018-04-15 03:09:27 -0400

    Got Tesseract Working

--- MediScan/SearchViewController.swift
+++ MediScan/SearchViewController.swift
@@ -7,16 +7,51 @@
 //
 
 import UIKit
+import TesseractOCR
+import Foundation
 
-class SearchViewController: UIViewController, UITextFieldDelegate {
+class SearchViewController: UIViewController {
 
     @IBOutlet weak var drug_name: UITextField!
     @IBOutlet weak var result: UILabel!
     var drug_list = Drugs()
+    @IBOutlet weak var inputt: UITextView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return (true)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    @IBAction func take_photo(_ sender: UIButton) {
+        presentImagePicker()
+        view.endEditing(true)
+    }
+    
+    func performImageRecognition(_ image: UIImage) {
+        // 1
+        if let tesseract = G8Tesseract(language: "eng") {
+            // 2
+            tesseract.engineMode = .tesseractCubeCombined
+            // 3
+            tesseract.pageSegmentationMode = .auto
+            // 4
+            tesseract.image = image.g8_blackAndWhite()
+            // 5
+            tesseract.recognize()
+            // 6
+            inputt.text = tesseract.recognizedText
+        }
+        // 7
+        activityIndicator.stopAnimating()
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.drug_name.delegate = self
         drug_list.set_up_dictionary()
         // Do any additional setup after loading the view.
     }
@@ -28,14 +63,16 @@ class SearchViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func side_effects(_ sender: UIButton) {
         let title = sender.title(for: .normal)!
-        let user_input: String = drug_name.text!
+        let user_input: String = inputt.text!
+        let trimmed = user_input.trimmingCharacters(in: .whitespacesAndNewlines)
+        print(user_input)
         let title2: String
-        if(drug_list.drug_in_database(name: user_input.uppercased())) {
+        if(drug_list.drug_in_database(name: trimmed.uppercased())) {
             if(title == "Side Effects") {
-                title2=drug_list.get_side_effects(name: user_input.uppercased())
+                title2=drug_list.get_side_effects(name: trimmed.uppercased())
             }
             else {
-                title2 = drug_list.get_main_usages(name: user_input.uppercased())
+                title2 = drug_list.get_main_usages(name: trimmed.uppercased())
             }
             result.text = title2
         }
@@ -43,13 +80,7 @@ class SearchViewController: UIViewController, UITextFieldDelegate {
             result.text = "Drug not found in database"
         }
     }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        self.view.endEditing(true)
-        return (true)
-    }
+    
     
     /*
     // MARK: - Navigation
@@ -62,3 +93,80 @@ class SearchViewController: UIViewController, UITextFieldDelegate {
     */
 
 }
+
+extension SearchViewController: UINavigationControllerDelegate {
+}
+
+// Set up taking a picture
+// MARK: - UIImagePickerControllerDelegate
+extension SearchViewController: UIImagePickerControllerDelegate {
+    func presentImagePicker() {
+        // 2
+        let imagePickerActionSheet = UIAlertController(title: "Snap/Upload Image",
+                                                       message: nil, preferredStyle: .actionSheet)
+        // 3
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let cameraButton = UIAlertAction(title: "Take Photo",
+                                             style: .default) { (alert) -> Void in
+                                                let imagePicker = UIImagePickerController()
+                                                imagePicker.delegate = self
+                                                imagePicker.sourceType = .camera
+                                                self.present(imagePicker, animated: true)
+            }
+            imagePickerActionSheet.addAction(cameraButton)
+        }
+        // 1
+        let libraryButton = UIAlertAction(title: "Choose Existing",
+                                          style: .default) { (alert) -> Void in
+                                            let imagePicker = UIImagePickerController()
+                                            imagePicker.delegate = self
+                                            imagePicker.sourceType = .photoLibrary
+                                            self.present(imagePicker, animated: true)
+        }
+        imagePickerActionSheet.addAction(libraryButton)
+        // 2
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel)
+        imagePickerActionSheet.addAction(cancelButton)
+        // 3
+        present(imagePickerActionSheet, animated: true)
+    }
+    
+    // 1
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // 2
+        if let selectedPhoto = info[UIImagePickerControllerOriginalImage] as? UIImage,
+            let scaledImage = selectedPhoto.scaleImage(640) {
+            // 3
+            activityIndicator.startAnimating()
+            // 4
+            dismiss(animated: true, completion: {
+                self.performImageRecognition(scaledImage)
+            })
+        }
+    }
+}
+
+// MARK: - UIImage extension
+extension UIImage {
+    func scaleImage(_ maxDimension: CGFloat) -> UIImage? {
+        
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        
+        if size.width > size.height {
+            let scaleFactor = size.height / size.width
+            scaledSize.height = scaledSize.width * scaleFactor
+        } else {
+            let scaleFactor = size.width / size.height
+            scaledSize.width = scaledSize.height * scaleFactor
+        }
+        
+        UIGraphicsBeginImageContext(scaledSize)
+        draw(in: CGRect(origin: .zero, size: scaledSize))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage
+    }
+}
+
